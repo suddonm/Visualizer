97b8fdd036dd6b41a22e31514041fef5c9bf40bb
suddonm <suddonm@gmail.com>
2022-11-13 14:28:17 -0800

    init

---
+++ Assets/Scripts/AudioWaveform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioWaveform : MonoBehaviour
+{
+    public GameObject myprefab;
+    public double threshold = 0.006;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float[] spectrum = new float[256];
+
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+
+        for (int i = 1; i < spectrum.Length; i+=24)
+        {
+            Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
+            Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
+            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+
+            if (spectrum[i] > threshold)
+            {
+                Instantiate(myprefab, new Vector3(15,0,10), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.Networking;
+
+public class UIController : MonoBehaviour
+{
+    string path;
+    public AudioClip audio;
+    public Button fileExplorer;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        fileExplorer.onClick.AddListener(GetSong);    
+    }
+
+    void GetSong()
+    {
+        path = EditorUtility.OpenFilePanel("Choose audio file", "", "mp3");
+
+        if (path != null)
+        {
+            UpdateSong();
+        }
+    }
+
+    void UpdateSong()
+    {
+        Debug.Log("Update");
+
+         using (var uwr = UnityWebRequestMultimedia.GetAudioClip("file://" + path, AudioType.AUDIOQUEUE))
+            {
+                ((DownloadHandlerAudioClip)uwr.downloadHandler).streamAudio = true;
+
+ 
+                DownloadHandlerAudioClip dlHandler = (DownloadHandlerAudioClip)uwr.downloadHandler;
+ 
+                if (dlHandler.isDone)
+                {
+                    AudioClip audioClip = dlHandler.audioClip;
+ 
+                    if (audioClip != null)
+                    {
+                        AudioSource player = (AudioSource)GetComponent(typeof(AudioSource));
+                        player.clip = audioClip;
+
+                        player.Play();
+ 
+                        Debug.Log("Playing song using Audio Source!");
+                       
+                    }
+                    else
+                    {
+                        Debug.Log("Couldn't find a valid AudioClip :(");
+                    }
+                }
+                else
+                {
+                    Debug.Log("The download process is not completely finished.");
+                }
+            }
+    }
+}
